f1fb89c6595429b9455092ec07abced8a274da5d
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2022-04-29 13:35:43 +0300

    Add project files.

---
+++ UrunYonetim.Entites/Kategori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrunYonetimi.Entites
+{
+    public class Kategori : IEntity
+    {
+        public int id { get; set; }
+        public string KategoriAdi { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public bool AktifMi { get; set; }
+    }
+}
---
+++ UrunYonetim.Entites/Urun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrunYonetimi.Entites
+{
+    internal class Urun : IEntity
+    {
+        public int id { get; set; }
+        public int KategoriId { get; set; }
+        public int MarkaId { get; set; }
+        public string UrunAdi { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public bool AktifMi { get; set; }
+        public decimal UrunFiyati { get; set; }
+        public int Kdv { get; set; }
+        public int StokMiktari { get; set; }
+    }
+
+
+}
